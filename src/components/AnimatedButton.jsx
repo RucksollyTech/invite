5ff8d4bd8826d81e392757d583332e7436566b5f
Rucksolly <rucksolly.tech@gmail.com>
2025-10-26 02:48:51 +0200

    Added link

--- src/components/AnimatedButton.jsx
+++ src/components/AnimatedButton.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
 export default function AnimatedButton() {
@@ -25,15 +26,23 @@ export default function AnimatedButton() {
       if (buttonRef.current) observer.unobserve(buttonRef.current);
     };
   }, []);
+  
 
   return (
-    <button
-      ref={buttonRef}
-      className={`relative bg-green-700 cursor-pointer text-white md:text-5xl text-xl font-extrabold md:py-6 py-4 md:px-8 px-5 rounded-2xl shadow-lg transition-all duration-300 hover:bg-green-800 ${
-        animate ? "animate-celebrate" : ""
-      }`}
+    <Link
+        href="https://t.me/+tnx-XVb-_GtjN2Q0"
+        target="_blank"
+        rel="noopener noreferrer"
     >
-      Join Our Telegram Channel for free
-    </button>
+        <button
+            onClick={()=>"https://t.me/+tnx-XVb-_GtjN2Q0"}
+            ref={buttonRef}
+            className={`relative bg-green-700 cursor-pointer text-white md:text-5xl text-xl font-extrabold md:py-6 py-4 md:px-8 px-5 rounded-2xl shadow-lg transition-all duration-300 hover:bg-green-800 ${
+                animate ? "animate-celebrate" : ""
+            }`}
+        >
+            Join Our Telegram Channel for free
+        </button>
+    </Link>
   );
 }
